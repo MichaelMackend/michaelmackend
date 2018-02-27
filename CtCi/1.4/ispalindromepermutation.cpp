7c0438b105b9475626851061f97a0f9f1578d82f
= <=>
2018-02-26 23:33:01 -0800

    add autotools files to _template and set them up for 1.4

---
+++ CtCi/1.4/ispalindromepermutation.cpp
@@ -0,0 +1,14 @@
+#include "ispalindromepermutation.h"
+#include <string.h>
+#include <iostream>
+
+
+
+
+bool isPalindromePermutation(const char* str) {
+
+
+}
+
+
+
---
+++ CtCi/1.4/ispalindromepermutation.h
@@ -0,0 +1,6 @@
+#ifndef CTCI_1_4_ISPALINDROMEPERMUTATION
+#define CTCI_1_4_ISPALINDROMEPERMUTATION
+
+bool isPalindromePermutation(const char* str);
+
+#endif
