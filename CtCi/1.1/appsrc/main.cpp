bb01c88f75c6b09684121a3ebf7037c749141748
Michael Mackend <michaelmackend@outlook.com>
2018-03-19 14:31:41 -0700

    add appsrc

---
+++ CtCi/1.1/appsrc/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "isunique.h"
+
+void testString(const char* str) {
+    std::cout << "String: \"";
+    std::cout << str << "\" is ";
+    if(isUnique(str)) {
+        std::cout << "unique!" << std::endl;
+    } else {
+        std::cout << "NOT unique!" << std::endl;
+    }
+}
+
+
+int main(void) {
+
+    testString("hello");
+    testString("helpo");
+
+}
---
+++ CtCi/1.1/include/isunique.h
@@ -0,0 +1,6 @@
+#ifndef CTCI_1_1_ISUNIQUE
+#define CTCI_1_1_ISUNIQUE
+
+bool isUnique(const char* str);
+
+#endif
