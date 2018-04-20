b2240fd379816630a1058f5ee09fc5c8c3c145eb
Michael Mackend <michaelmackend@outlook.com>
2018-04-20 15:40:27 -0700

    update 1.1 and 1.2 to handle post / on 80

--- projects/services/hello/appsrc/main.cpp
+++ projects/services/hello/appsrc/main.cpp
@@ -8,44 +8,9 @@ using namespace httplib;
 using json = nlohmann::json;
 
 void get_hi(const Request& req, Response& res) {
-    json j;
-    j["response"] = "hello!";
-
-    std::cout << j.dump(4) << std::endl;
-
     res.set_content("Hello!", "text/plain");
 }
 
-void post(const Request& req, Response& res) {
-
-    std::cout << "POST!" << std::endl;
-    std::string body = req.body;
-
-    std::cout << "body: " << body << std::endl;
-
-    json j;
-    j = j.parse(body);
-
-    std::cout << "jbody: " << j.dump(4) << std::endl;
-
-    for(auto it = j.begin(); it != j.end(); ++it) {
-        std::cout << it.key() << " : " << it.value() << std::endl;
-    }
-
-    auto keyiter = j.find("word");
-    if(keyiter != j.end()) {
-        std::string word = *keyiter;
-        std::cout << "I want to know about " << word << std::endl;
-
-        bool result = isUnique(word.c_str());
-
-        json j;
-        j["name"] = word;
-        j["isUnique"] = result;
-
-        res.set_content( j.dump(), "application/json");
-    }
-}
 
 int main(void) {
 
@@ -55,9 +20,7 @@ int main(void) {
 
     svr.get("/hi", get_hi);
 
-    svr.post("/", post);
-
-    svr.listen("0.0.0.0", 13370);
+    svr.listen("0.0.0.0",80);
 
     return 0;
 }
