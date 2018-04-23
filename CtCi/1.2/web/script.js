a7bb640431c61ae410019defdca132f2045f1624
Michael Mackend <michael@obelisk-16.04>
2018-04-23 00:32:23 -0700

    configure a website and route for 1.2. done.

---
+++ CtCi/1.2/web/script.js
@@ -0,0 +1,36 @@
+(function ($) {
+    $.fn.serializeFormJSON = function () {
+
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function () {
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+})(jQuery);
+
+$(document).ready(function() {
+
+$('form').submit(function(event) {
+    event.preventDefault();
+    var form = $( this ).serializeFormJSON();
+    var formstring = JSON.stringify(form);
+    $.post("/ctci/1.2/", formstring, function(data) {
+       console.log(data);
+       if(data.isPermutation) {
+           $('.results').html("PERMUTATIONS!");
+       } else {
+           $('.results').html("Not permutations.");
+       }
+    });
+});
+
+});
