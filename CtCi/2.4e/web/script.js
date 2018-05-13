99f70b1ef6ef978d98ceb6d41df85ea46f96f036
Michael Mackend <michaelmackend@outlook.com>
2018-05-12 23:41:11 -0700

    finish up 2.4e

--- CtCi/2.4e/web/script.js
+++ CtCi/2.4e/web/script.js
@@ -24,78 +24,16 @@ $('form').submit(function(event) {
     event.preventDefault();
     var form = $( this ).serializeFormJSON();
     var formstring = JSON.stringify(form);
-    $.post("/ctci/1.?/", formstring, function(data) {
+    $.post("/ctci/2.4e/", formstring, function(data) {
        console.log(data);
-       if("return" in data) {
-           $('.results').html(data.return);
+       if("sum" in data) {
+           $('#result').html(data.sum);
        } else {
-           $('.results').html("Something went wrong.");
+           $('#result').html("Something went wrong.");
        }
     });
 });
 
 });
-
-function onRotateMatrixClicked() {
-    var grid = document.querySelector('data-grid');
-    var data = grid.getData();
-    var formstring = JSON.stringify(data);
-    $.post("/ctci/1.7/", formstring, function(response) {
-        grid.setData(response);
-    })
-    .done(function() {
-        console.log("second success" );
-    })
-    .fail(function() {
-        console.log( "error" );
-    })
-    .always(function() {
-        console.log( "finished" );
-    });
-}
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-function onGenerateTable() {
-    var input = document.querySelector("input");
-    var size = input.valueAsNumber;
-
-    if(size > 20) {
-        size = 20;
-    }
-
-    if(size < 1) {
-        size = 1;
-    }
-
-    input.value = size;
-
-    var grid = document.createElement("data-grid");
-    grid.setAttribute("rows",0);
-    grid.style.width = "400px";
-    grid.style.height = "400px";
-
-
-    var parent = document.querySelector("#gridHolder");
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
-    parent.appendChild(grid);
-
-    var data = {
-        gridSize: size,
-        values: []
-    };
-
-    var count = size * size;
-    for(var i = 0; i < count; ++i) {
-        data.values.push(getRandomInt(10));
-    }
-
-    grid.setData(data);
-}
-
 
 
