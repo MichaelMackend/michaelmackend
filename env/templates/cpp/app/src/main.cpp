418312e72ec9f4f6e1ce23993dc28e0a10e2bab4
Michael Mackend <michaelmackend@outlook.com>
2018-11-04 01:40:44 -0700

    added cpp app template

---
+++ env/templates/cpp/app/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <sstream>
+#include <exception>
+#include <string>
+#include <vector>
+
+int main(void) {
+
+    std::cout << "Hello, world!" << std::endl;
+
+    return 0;
+}
+
+
